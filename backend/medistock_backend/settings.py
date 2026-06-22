a5869b0af312ac1db5fde6f6b6ffd0d6891ba95c
Ricardo Antonio Aravena Celis <133412853+rikenichi@users.noreply.github.com>
2026-06-22 17:31:22 -0400

    Habilita CORS para frontend local

--- backend/medistock_backend/settings.py
+++ backend/medistock_backend/settings.py
@@ -24,11 +24,14 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
 
+    'corsheaders',
+
     'accounts',
     'inventory',
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+
 ]
 
 ROOT_URLCONF = 'medistock_backend.urls'
@@ -85,3 +89,8 @@ REST_FRAMEWORK = {
 }
 
 STATIC_URL = "static/"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
